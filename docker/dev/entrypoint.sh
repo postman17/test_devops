4f3226bbc5b615698cac356aa409c28581ce1cd2
postman17 <frompostal@gmail.com>
2019-02-01 18:09:48 +0500

    add part of docker

---
+++ docker/dev/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -x
+echo "Starting backend ..."
+
+if [[ ${POSTGRES_HOST} ]]; then
+    export PGPASSWORD=${POSTGRES_PASSWORD}
+    until psql -h $POSTGRES_HOST -U $POSTGRES_USER -d $POSTGRES_NAME -c '\l'; do
+      >&2 echo "Postgres is unavailable - sleeping"
+      sleep 1
+    done
+    >&2 echo "Postgres is up - continuing"
+    psql -h $POSTGRES_HOST -U $POSTGRES_USER -d template1 -c 'create extension hstore;'
+    unset PGPASSWORD
+fi
+
+
+python manage.py collectstatic --noinput
+#python manage.py loaddata fixtures.json
+python manage.py migrate --noinput
+gunicorn -b 0:8028 conf.wsgi:application
+
+echo "End"
